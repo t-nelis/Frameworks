d390c0774bd025aadd8c66dec8d4b9bc97e6547a
DaniilVeriga <daniil.veriga@gmail.com>
2014-08-06 21:36:47 +0400

    [UPDATE] jQuery Length should be a field, not a property.

--- jQuery2/Miscellaneous/Instances.cs
+++ jQuery2/Miscellaneous/Instances.cs
@@ -1,16 +1,12 @@
-﻿namespace Bridge.jQuery2
+﻿using Bridge.CLR;
+
+namespace Bridge.jQuery2
 {    
     public partial class jQuery
     {
         /// <summary>
         /// The number of elements in the jQuery object.
         /// </summary>
-        public virtual int Length
-        {
-            get
-            {
-                return 0;
-            }
-        }
+        public readonly int Length;
     }
 }
