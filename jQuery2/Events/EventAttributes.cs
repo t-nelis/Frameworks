8ba991535c3bf1a9426026ca6f2e17c24f0a284b
Vladimir Scheglov <vscheglov@gmail.com>
2014-12-22 19:08:33 +0300

    [ADD] JQuery event attribute support

---
+++ jQuery2/Events/EventAttributes.cs
@@ -0,0 +1,51 @@
+﻿using Bridge.CLR;
+
+namespace Bridge.jQuery2
+{
+    [Ignore]
+    public enum JqueryEventTarget
+    {
+        [Template("{0}")]
+        Raw=0,
+
+        [Template("'{0}'")]
+        Value
+    }
+
+    [Ignore]
+    public abstract class EventAttribute : Bridge.CLR.EventAttribute
+    {
+        public const string Format = "$({1}).on('{0}', this.{2});";
+    }
+
+    [Ignore]
+    public class JQueryEventAttribute : EventAttribute
+    {
+        public const bool IsCommonEvent = true;
+
+        public JQueryEventAttribute(string eventName, string selector, JqueryEventTarget target = JqueryEventTarget.Value)
+        {
+        }
+    }  
+
+    [Ignore]
+    public class ReadyAttribute : EventAttribute
+    {
+        public const string Format = "$(this.{2});";
+        public const string Event = "ready";
+
+        public ReadyAttribute()
+        {            
+        }
+    }
+
+    [Ignore]
+    public class ClickAttribute : EventAttribute
+    {
+        public const string Event = "click";
+
+        public ClickAttribute(string selector, JqueryEventTarget target = JqueryEventTarget.Value)
+        {
+        }
+    } 
+}
