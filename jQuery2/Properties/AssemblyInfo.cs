74390b41c6a60c6650422bfac2c3298c7766d59d
Geoffrey McGill <geoff@object.net>
2015-03-30 01:06:20 -0600

    Removed -beta flag from version number

--- jQuery2/Properties/AssemblyInfo.cs
+++ jQuery2/Properties/AssemblyInfo.cs
@@ -12,4 +12,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("92c5fd07-ea24-489d-8fe4-692cab8ab447")]
 [assembly: AssemblyVersion("2.0")]
-[assembly: AssemblyFileVersion("2.0.0-beta")]
+[assembly: AssemblyFileVersion("2.0.0")]
