928294a5abce45b84fdccdbbb964d4fa26ec814c
Geoffrey McGill <geoff@object.net>
2015-04-12 22:45:42 -0600

    Update assembly version numbers

--- Bootstrap3/Properties/AssemblyInfo.cs
+++ Bootstrap3/Properties/AssemblyInfo.cs
@@ -12,4 +12,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("ebd43cb4-1057-4dc5-9a7e-78c96c08b2c9")]
 [assembly: AssemblyVersion("3.0")]
-[assembly: AssemblyFileVersion("3.0.0")]
+[assembly: AssemblyFileVersion("3.0.1")]
