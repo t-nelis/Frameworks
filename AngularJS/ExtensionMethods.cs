43c981b30c96f139185ad75cf78f5229b4cac629
Fabrício Zimmerer Murta <fabricio@object.net>
2015-08-21 22:45:10 -0300

    Creates ngModel extension support for the Element class. [#1]

--- AngularJS/ExtensionMethods.cs
+++ AngularJS/ExtensionMethods.cs
@@ -8,5 +8,25 @@ namespace Bridge.AngularJS
     [Ignore]
     public static partial class AngularJSExtensions
     {
+        /// <summary>
+        /// Sets the NG model reference to the specified element.
+        /// </summary>
+        /// <param name="el">Element to get the ngModel reference.</param>
+        /// <param name="modelName">Name for the model applied.</param>
+        [Template("{el}.setAttribute('ng-model', {modelName})")]
+        public static void setNGModel(this InputElement el, string modelName)
+        {
+        }
+
+        /// <summary>
+        /// Drops the NG model attribute from the element.
+        /// </summary>
+        /// <param name="el">
+        /// Element to have the ngModel attribute removed.
+        /// </param>
+        [Template("{el}.removeAttribute('ng-model')")]
+        public static void dropNGModel(this InputElement el)
+        {
+        }
     }
 }
