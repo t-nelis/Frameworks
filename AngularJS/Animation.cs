19188bfae1c18b7a6bbcd17824990249a54c5eed
Fabrício Zimmerer Murta <fabricio@object.net>
2015-09-15 23:50:53 -0300

    Implement the Module class. [#1]

---
+++ AngularJS/Animation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Bridge.AngularJS
+{
+    public class Animation
+    {
+        // Function which returns a function to cancel animation
+        public Func<Action> EventName;
+
+        public Animation()
+        {
+        }
+    }
+}
+
--- AngularJS/Module.cs
+++ AngularJS/Module.cs
@@ -5,7 +5,120 @@ namespace Bridge.AngularJS
     [Ignore]
     public class Module
     {
+        #region Fields
+        /// <summary>
+        /// Holds the list of modules which the injector will load
+        /// before the current module is loaded.
+        /// </summary>
+        public string[] Requires;
+
+        /// <summary>
+        /// The name of the module.
+        /// </summary>
+        public string Name;
+        #endregion
+
+        #region Methods
         public Module()
+        {
+        }
+
+        /// <summary>
+        /// Register a provider function with the $injector.
+        /// </summary>
+        /// <param name="name">Service name.</param>
+        /// <param name="providerType">
+        /// Construction function for creating new instance of the service.
+        /// </param>
+        public void Provider(string name, Action providerType)
+        {
+        }
+
+        /// <summary>
+        /// Register a service factory, which will be called to return
+        /// the service instance.
+        /// </summary>
+        /// <param name="name">Service name.</param>
+        /// <param name="providerFunction">
+        /// Function for creating new instance of the service.
+        /// </param>
+        public void Factory(string name, Action providerFunction)
+        {
+        }
+
+        /// <summary>
+        /// Register a service constructor, which will be invoked with
+        /// new to create the service instance.
+        /// </summary>
+        /// <param name="name">Service name.</param>
+        /// <param name="constructor">
+        /// A constructor function that will be instantiated.
+        /// </param>
+        public void Service(string name, Action constructor)
+        {
+        }
+
+        /// <summary>
+        /// Register a value service with the $injector, such as a string,
+        /// a number, an array, an object or a function.
+        /// </summary>
+        /// <param name="name">Service name.</param>
+        /// <param name="instanceObject">Service instance object.</param>
+        public void Value(string name, object instanceObject)
+        {
+        }
+
+        /// <summary>
+        /// Register a constant service, such as a string, a number,
+        /// an array, an object or a function, with the $injector.
+        /// </summary>
+        /// <param name="name">Constant name.</param>
+        /// <param name="constantValue">Constant value.</param>
+        /// <remarks>
+        /// Because the constant are fixed, they get applied before
+        /// other provide methods.
+        /// </remarks>
+        public void Constant(string name, object constantValue)
+        {
+        }
+
+        /// <summary>
+        /// Register a service decorator with the $injector.
+        /// </summary>
+        /// <param name="the">Name of service to decorate.</param>
+        /// <param name="instantiateFunction">
+        /// Function will be invoked when the service needs to be instantiated
+        /// and should return the decorated service instance.
+        /// </param>
+        public void Decorator(string the, Action instantiateFunction)
+        {
+        }
+
+        /// <summary>
+        /// Defines an animation hook that can be later used with $animate
+        /// service and directives that use this service.
+        /// </summary>
+        /// <remarks>>
+        /// Animations take effect only if the ngAnimate module is loaded.
+        /// </remarks>
+        /// <param name="name">Animantion name.</param>
+        /// <param name="animationFactory">
+        /// Factory function for creating new instance of an animation.
+        /// </param>
+        public void Animation(string name, Func<Animation> animationFactory)
+        {
+        }
+
+        /// <summary>
+        /// Define a filter using the specified name and filterFactory.
+        /// </summary>
+        /// <param name="name">
+        /// Filter name - this must be a valid angular expression identifier.
+        /// </param>
+        /// <param name="filterFactory">
+        /// Factory function for creating new instance of filter.
+        /// </param>
+        public void Filter(string name, Action filterFactory)
         {
         }
 
@@ -66,11 +179,46 @@ namespace Bridge.AngularJS
         //  template object property!)
 
         /// <summary>
-        /// Applies the builder function to the
+        /// Register a new directive with the compiler.
         /// </summary>
         /// <param name="directiveName"></param>
         /// <param name="builder"></param>
         public void Directive(string directiveName, Func<object> builder)
+        {
+        }
+        #endregion
+
+        /// <summary>
+        /// Register work which needs to be performed on module loading.
+        /// </summary>
+        /// <param name="configFn">
+        /// Function to be executed on module load.
+        /// </param>
+        /// <typeparam name="T">
+        /// The type of the argument passed to the function.
+        /// </typeparam>
+        /// <seealso cref="Provider Recipe"/>
+        /// <remarks>
+        /// Useful for service configuration.
+        /// </remarks>
+        public void Config<T>(Action<T> configFn)
+        {
+        }
+
+        /// <summary>
+        /// Register work which should be performed when the injector
+        /// is done loading all modules.
+        /// </summary>
+        /// <param name="initializationFn">
+        /// Function to be executed after injector creation.
+        /// </param>
+        /// <typeparam name="T">
+        /// The type argument passed to the function.
+        /// </typeparam>
+        /// <remarks>
+        /// Useful for application initialization.
+        /// </remarks>
+        public void Run<T>(Action<T> initializationFn)
         {
         }
         #endregion
