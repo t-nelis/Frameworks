8ac70a12bb1d56e59be39a609c9af39689d8065d
Geoffrey McGill <geoff@object.net>
2015-09-21 09:42:24 -0600

    Update version numbers

--- Collections/Properties/AssemblyInfo.cs
+++ Collections/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2a995530-b40e-42b5-a949-014c1186967e")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
