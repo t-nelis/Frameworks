3b01bdbbbef7f415ea81a93342bd94d8c58e2f26
Fabrício Zimmerer Murta <fabricio@object.net>
2015-10-25 10:20:08 -0200

    Updates Animation service to expect jqLite references. [#1]

--- AngularJS/Animation.cs
+++ AngularJS/Animation.cs
@@ -1,18 +1,19 @@
-﻿using Bridge.Html5;
-using System;
+﻿using System;
 
 namespace Bridge.AngularJS
 {
+    // TODO: Overload / extension with jQuery on separate project which requires
+    //       jQuery to be included.
     [Ignore]
     [ObjectLiteral]
     public class Animation
     {
         // Function which returns a function to cancel animation
-        public Func<Element, Action, Action<Element>> EventName;
+        public Func<jqLite, Action, Action<jqLite>> EventName;
 
-        public Func<Element, string, Action, Action<bool>> AddClass;
+        public Func<jqLite, string, Action, Action<bool>> AddClass;
 
-        public Func<Element, string, Action, Action<bool>> RemoveClass;
+        public Func<jqLite, string, Action, Action<bool>> RemoveClass;
     }
 }
 
