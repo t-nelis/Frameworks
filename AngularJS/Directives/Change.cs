b92851acbb862624b8b36489648a11689b6167ee
Fabrício Zimmerer Murta <fabricio@object.net>
2015-11-30 17:50:49 -0200

    Creates the Directive and adapt the 'Change' directive. [#25]

--- AngularJS/Directives/Change.cs
+++ AngularJS/Directives/Change.cs
@@ -1,52 +1,41 @@
-﻿using Bridge.Html5;
-
-namespace Bridge.AngularJS
+﻿namespace Bridge.AngularJS
 {
     /// <summary>
     /// Extension methods for the ngChange directive.
     /// </summary>
-    public static class ChangeDirectives
+    [External]
+    public partial class AngularElement
     {
         /// <summary>
         /// Gets the current textual value of ng-change (ngChange)
         /// directive bound to the element, if any.
         /// </summary>
-        /// <param name="el"></param>
         /// <returns>String value the element currently has.</returns>
         /// <see cref="!:https://docs.angularjs.org/api/ng/directive/ngChange">
         /// Official JavaScript documentation.
         /// </see>
-        [Template("{el}.getAttribute('ng-change')")]
-        public static string GetNGChange(this Element el)
-        {
-            return default(string);
-        }
+        [Template("{this}.getAttribute('ng-change')")]
+        public extern string GetChange();
         
         /// <summary>
         /// Sets a textual value to the ng-change (ngChange)
         /// directive on the element.
         /// </summary>
-        /// <param name="el"></param>
         /// <param name="value">Value to bind to the directive.</param>
         /// <see cref="!:https://docs.angularjs.org/api/ng/directive/ngChange">
         /// Official JavaScript documentation.
         /// </see>
-        [Template("{el}.setAttribute('ng-change', {value})")]
-        public static void SetNGChange(this Element el, string value)
-        {
-        }
-        
+        [Template("{this}.setAttribute('ng-change', {value})")]
+        public extern void SetChange(string value);
+
         /// <summary>
         /// Removes the ng-change (ngChange) directive definition
         /// from the element.
         /// </summary>
-        /// <param name="el"></param>
         /// <see cref="!:https://docs.angularjs.org/api/ng/directive/ngChange">
         /// Official JavaScript documentation.
         /// </see>
-        [Template("{el}.removeAttribute('ng-change')")]
-        public static void RemoveNGChange(this Element el)
-        {
-        }
+        [Template("{this}.removeAttribute('ng-change')")]
+        public extern void RemoveChange();
     }
 }
---
+++ AngularJS/Directives/Directives.cs
@@ -0,0 +1,13 @@
+﻿using Bridge.Html5;
+
+namespace Bridge.AngularJS
+{
+    public static class Directives
+    {
+        [Template("{el}")]
+        public extern static AngularElement NG(this Element el);
+
+        [Template("{el}")]
+        public extern static AngularElement Angular(this Element el);
+    }
+}
