373d61edaf3bd4c250e7f23cb994c42bdf4b47a6
Fabrício Zimmerer Murta <fabricio@object.net>
2015-11-30 16:57:01 -0200

    Removes a duplicate occurrence of Repeat.cs file. [#24]

--- AngularJS/Directives/Repeat.cs
+++ AngularJS/Directives/Repeat.cs
@@ -48,5 +48,92 @@ namespace Bridge.AngularJS
         public static void RemoveNGRepeat(this Element el)
         {
         }
+
+        /// <summary>
+        /// Sets the NG repeat to the element with the specified free text
+        /// expression.
+        /// </summary>
+        /// <param name="el">Element to apply the ngRepeat to</param>
+        /// <param name="expression">Valid ngRepeat AngularJS expression</param>
+        /// <remarks>Must be used inside a ngController scope</remarks>
+        [Template("{el}.setAttribute('ng-repeat', {expression})")]
+        public static void SetNGRepeat(this Element el, string expression)
+        {
+        }
+
+        /// <summary>
+        /// Sets the NG repeat to the element with the specified iteration
+        /// variable and iterated array names.
+        /// </summary>
+        /// <param name="el">Element to apply ngRepeat to</param>
+        /// <param name="variable">Iteration variable</param>
+        /// <param name="array">Existing array in the scoped controller.</param>
+        /// <remarks>Must be used inside a ngController scope</remarks>
+        [Template("{el}.setAttribute('ng-repeat', " +
+            "'' + {variable} + ' in ' + {array})")]
+        public static void SetNGRepeat(this Element el, string variable,
+            string array)
+        {
+        }
+
+        // FIXME: There are much more over Filter and OrderBy. Another approach
+        //        will have to be taken.
+        /// <summary>
+        /// Sets the NG repeat to the element with the specified iteration
+        /// variable, iterated array and filter input field.
+        /// </summary>
+        /// <param name="el">Element to apply ngRepeat to</param>
+        /// <param name="variable">Iteration variable</param>
+        /// <param name="array">Existing array in the scoped controller.</param>
+        /// <param name="filterInputField">Filter input field.</param>
+        /// <remarks>
+        /// Must be used inside a ngController scope and input field for filter
+        /// must be defined somewhere else in the page
+        /// </remarks>
+        [Template("{el}.setAttribute('ng-repeat', " +
+                  "'' + {variable} + ' in ' + {array} + ' | " +
+                  "filter:' + {filterInputField})")]
+        public static void SetNGRepeatWithFilter(this Element el,
+            string variable, string array, string filterInputField)
+        {
+        }
+
+        /// <summary>
+        /// Sets the NG repeat to the element with the specified iteration
+        /// variable, iterated array, filter input and order input fields.
+        /// </summary>
+        /// <param name="el">Element to apply ngRepeat to.</param>
+        /// <param name="variable">Iteration variable.</param>
+        /// <param name="array">Existing array in the scoped controller.</param>
+        /// <param name="filterInputField">Filter input field.</param>
+        /// <param name="orderByInputField">Order by input field.</param>
+        /// <remarks>
+        /// Must be used inside a ngController scope and input field for filter
+        /// must be defined somewhere else in the page
+        /// </remarks>
+        [Template("{el}.setAttribute('ng-repeat', " +
+                  "'' + {variable} + ' in ' + {array} + ' | " +
+                  "filter: ' + {filterInputField} + ' | " +
+                  "orderBy: ' + {orderByInputField})")]
+        public static void SetNGRepeat(this Element el, string variable,
+            string array, string filterInputField, string orderByInputField)
+        {
+        }
+
+        /// <summary>
+        /// Sets the NG repeat to the element with the specified iteration
+        /// variable, iterated array and OrderBy input field.
+        /// </summary>
+        /// <param name="el">El.</param>
+        /// <param name="variable">Variable.</param>
+        /// <param name="array">Array.</param>
+        /// <param name="orderByInputField">Order by input field.</param>
+        [Template("{el}.setAttribute('ng-repeat', " +
+                  "'' + {variable} + ' in ' + {array} + ' | " +
+                  "orderBy: ' + {orderByInputField})")]
+        public static void SetNGRepeatWithOrderBy(this Element el,
+            string variable, string array, string orderByInputField)
+        {
+        }
     }
 }
