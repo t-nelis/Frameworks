4d0f07f027ef8ff2a79c74c46d4bb366d8a9f48e
Fabrício Zimmerer Murta <fabricio@object.net>
2015-11-30 17:19:40 -0200

    Removes a duplicate occurrence of SetNGRepeat. [#24]

--- AngularJS/Directives/Repeat.cs
+++ AngularJS/Directives/Repeat.cs
@@ -23,20 +23,6 @@ namespace Bridge.AngularJS
         }
         
         /// <summary>
-        /// Sets a textual value to the ng-repeat (ngRepeat)
-        /// directive on the element.
-        /// </summary>
-        /// <param name="el"></param>
-        /// <param name="value">Value to bind to the directive.</param>
-        /// <see cref="!:https://docs.angularjs.org/api/ng/directive/ngRepeat">
-        /// Official JavaScript documentation.
-        /// </see>
-        [Template("{el}.setAttribute('ng-repeat', {value})")]
-        public static void SetNGRepeat(this Element el, string value)
-        {
-        }
-        
-        /// <summary>
         /// Removes the ng-repeat (ngRepeat) directive definition
         /// from the element.
         /// </summary>
