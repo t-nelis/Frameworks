cfbc1c808bff5085f6083c3c245d79c8f6d779ef
Fabrício Zimmerer Murta <fabricio@object.net>
2015-12-03 14:29:41 +0100

    Adds the promise in the format $http service exports. [#28]

--- AngularJS/Services/Http.cs
+++ AngularJS/Services/Http.cs
@@ -14,6 +14,64 @@ namespace Bridge.AngularJS.Services
     }
 
     /// <summary>
+    /// $http service returns a slightly different promise with Success()
+    /// and Error() chainings.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    [External]
+    public class HttpPromise<T> : Promise<T>
+    {
+        /// <summary>
+        /// Shorthand for Then(callback, null, null).
+        /// </summary>
+        /// <param name="callback">Callback function called on success.</param>
+        /// <returns>$http Service promise.</returns>
+        public extern HttpPromise<T> Success(Func<T> callback);
+
+        /// <summary>
+        /// Alias for Catch(callback).
+        /// </summary>
+        /// <param name="callback">Callback function called on error.</param>
+        /// <returns>$http Service promise.</returns>
+        public extern HttpPromise<T> Error(Func<T> callback);
+
+        /// <summary>
+        /// Regardless of when the promise was or will be resolved or rejected,
+        /// 'Then' calls one of the success or error callbacks asynchronously
+        /// as soon as the result is available. The callbacks are called with a
+        /// single argument: the result or rejection reason. Additionally, the
+        /// notify callback may be called zero or more times to provide a
+        /// progress indication, before the promise is resolved or rejected.
+        /// </summary>
+        /// <param name="successCallback">Called on success.</param>
+        /// <param name="errorCallback">Called on error.</param>
+        /// <param name="notifyCallback">Called zero or more times.</param>
+        /// <returns>
+        /// A new promise which is resolved or rejected via the return value of
+        /// the successCallback, errorCallback (unless that value is a promise,
+        /// in which case it is resolved with the value which is resolved in
+        /// that promise using promise chaining). It also notifies via the
+        /// return value of the notifyCallback method. The promise cannot be
+        /// resolved or rejected from the notifyCallback method.
+        /// </returns>
+        public new extern HttpPromise<T> Then(Func<T> successCallback, Func<T> errorCallback = null, Func<T> notifyCallback = null);
+
+        /// <summary>
+        /// A shorthand for Then(null, callback).
+        /// </summary>
+        /// <param name="callback">Called on error.</param>
+        /// <returns>
+        /// A new promise which is resolved or rejected via the return value of
+        /// the successCallback, errorCallback (unless that value is a promise,
+        /// in which case it is resolved with the value which is resolved in
+        /// that promise using promise chaining). It also notifies via the
+        /// return value of the notifyCallback method. The promise cannot be
+        /// resolved or rejected from the notifyCallback method.
+        /// </returns>
+        public new extern HttpPromise<T> Catch(Func<T> callback);
+    }
+
+    /// <summary>
     /// The interceptors are service factories that are registered with the
     /// $httpProvider by adding them to the $httpProvider.interceptors array.
     /// The factory is called and injected with dependencies (if specified)
